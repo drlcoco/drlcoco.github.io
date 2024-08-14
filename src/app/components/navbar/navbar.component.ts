14667fd060c022238b686dce0b0e12038ee56d12
David De los reyes luque <davidrl@MBP-de-David.home>
2024-08-14 06:31:24 +0200

    Fix nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  setActiveTab(tab: string): boolean {
+    return tab === this.router.url;
+  }
 }
