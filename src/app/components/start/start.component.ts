eaaf1f322091f206dd23ba45b3379d5459dcd622
David De los reyes luque <davidrl@MBP-de-David.home>
2024-08-15 03:17:50 +0200

    Fix nav 11

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [],
   templateUrl: './start.component.html',
-  styleUrl: './start.component.css'
+  styleUrls: ['./start.component.css']
 })
-export class StartComponent {
+export class StartComponent implements AfterViewInit {
 
+  @ViewChild('videoPlayer') videoPlayer?: ElementRef;
+
+  ngAfterViewInit() {
+    this.focus();
+    if (this.videoPlayer?.nativeElement) {
+      this.videoPlayer.nativeElement.addEventListener('play', () => {
+      });
+
+      this.videoPlayer.nativeElement.addEventListener('pause', () => {
+      });
+    } else {
+      console.error('El elemento videoPlayer no se encontró.');
+    }
+  }
+
+  focus() {
+    if (this.videoPlayer?.nativeElement) {
+      this.videoPlayer.nativeElement.focus();
+    } else {
+      console.error('El elemento videoPlayer no está disponible.');
+    }
+  }
 }
+
+
+
