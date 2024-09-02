6633cacc49a5800267ace115377c0c56f59bb1a7
System Administrator <root@MacBook-Pro-de-David.local>
2024-09-03 00:38:17 +0200

    Scroll effect 19

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 
 @Component({
@@ -7,12 +7,39 @@ import { RouterModule, Router } from '@angular/router';
   standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  public isHeaderVisible: boolean = true; // Cambiar a isHeaderVisible
+  private lastScrollTop: number = 0;
+  private scrollThreshold: number = 150;
+  isToggle: boolean = false;
+
   constructor(private router: Router) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > this.lastScrollTop && currentScrollTop > this.scrollThreshold) {
+      // Scrolling hacia abajo: esconder header
+      this.isHeaderVisible = false;
+    } else if (currentScrollTop < this.lastScrollTop) {
+      // Scrolling hacia arriba: mostrar header
+      this.isHeaderVisible = true;
+    }
+
+    // Actualizar la posición de scroll anterior
+    this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+  }
+
+  changeToggle() {
+    this.isToggle = !this.isToggle;
+  }
 
   setActiveTab(tab: string): boolean {
     return tab === this.router.url;
   }
 }
+
